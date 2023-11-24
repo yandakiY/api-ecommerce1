a32cc8d4ab5c006ec7dae514d8fe97aeba189f6f
yandaki <boayves@gmail.com>
2023-11-24 12:11:20 +0000

    implementation models and admin page

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Product
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id' , 'title' , 'slug' , 'created']
+    
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id' , 'title' , 'description' , 'created' , 'category' , 'old_price', 'image']
--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from utils.models import Model as ModelId
+from django_extensions.db.models import TitleSlugDescriptionModel , TimeStampedModel ,ActivatorModel
 
 # Create your models here.
+
+class Category(ModelId, TitleSlugDescriptionModel , TimeStampedModel , ActivatorModel):
+    
+    def __str__(self) -> str:
+        return f'{self.title}'
+
+    class Meta:
+        ordering = ['title']
+
+
+class Product(ModelId , TitleSlugDescriptionModel, TimeStampedModel , ActivatorModel):
+    
+    category = models.ForeignKey(Category , on_delete=models.CASCADE)
+    old_price = models.DecimalField(max_digits=16 , decimal_places=2)
+    stock = models.PositiveIntegerField(default=1)
+    discount = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='img/' , default='' , blank=True, null=True)
+    
+    @property
+    def price(self):
+        if self.discount:
+            new_price = self.old_price - ((30/100)*self.old_price)
+        else:
+            new_price = self.old_price
+        
+        return new_price
+    
+    @property
+    def img(self):
+        if self.image == '':
+            self.image = ''
+        
+        return self.image
+    
